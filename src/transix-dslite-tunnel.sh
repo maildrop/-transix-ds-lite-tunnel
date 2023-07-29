a435bee66e3e933791810338c8888a9aa9078ba9
TOGURO Mikito <mit@shalab.net>
2023-07-29 13:41:32 +0900

    起動時に tun0 の作成に失敗することがあったので、5秒の待機時間を導入 squid の起動にも失敗していたので、さらに5秒の待機時間を導入

--- src/transix-dslite-tunnel.sh
+++ src/transix-dslite-tunnel.sh
@@ -24,6 +24,7 @@ dslite_tunnel(){
             ip route add default via $v4addr dev $device table $table_name
             ip rule add from $v4addr table dslite$id
             ip address add $v4addr/$v4netmask dev $device
+            echo tunnel up finished. 
             ;;
         down)
             echo $verb $id $v4addr $local_addr $remote_addr
@@ -42,7 +43,9 @@ dslite_tunnel(){
 
 tunnel_id=0
 local_addr=$(ip -one -6 -color=never addr show scope global -dynamic up | awk '{ print $4 ; }' | sed 's/\/[[:digit:]]\+//g')
+/usr/bin/sleep 5s
 for remote_addr in $(host -t aaaa gw.transix.jp | awk '{print $5;}' ) ; do
     dslite_tunnel $1 ${tunnel_id} $local_addr $remote_addr
     tunnel_id=$(expr $tunnel_id + 1)
 done
+/usr/bin/sleep 5s
